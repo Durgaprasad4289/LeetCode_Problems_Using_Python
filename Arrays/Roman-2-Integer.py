10d1d8ba0a0c1621f3a8f0208a924600d32fb825
ANIL <durgaprasad04289@gmail.com>
2026-01-31 19:45:12 +0530

    Roman-2-Inger

--- Arrays/Roman-2-Integer.py
+++ Arrays/Roman-2-Integer.py
@@ -21,12 +21,9 @@
 
 class Solution(object):
     def romanToInt( self,s):
-    
         d={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-
         res=0
         prev=0
-
         for curr in s:
             if prev >d[curr]:
                 res-=d[curr]
