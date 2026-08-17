0bd608b6b3326c892a3f06a5d67620f7191a3dbe
Durga Prasad Kota <durgaprasad04289@gmail.com>
2026-08-17 22:05:46 +0530

    1588. Sum of All Odd Length Subarrays

--- HashMap + PrefixSum/Sum of All Odd Length Subarrays.py
+++ HashMap + PrefixSum/Sum of All Odd Length Subarrays.py
@@ -1,16 +1,17 @@
 class Solution:
-    def sumOddLengthSubarrays(self, arr):
-        total = 0
+    def sumOddLengthSubarrays(self, arr: List[int]) -> int:
 
-        for i in range(len(arr)):
-            current_sum = 0
+        # total = 0
+        # for i in range(len(arr)):
+        #     cur_sum = 0
+        #     for j in range(i,len(arr)):
+        #         cur_sum += arr[j]
+        #         if (j-i+1) % 2 != 0:
+        #             total += cur_sum
+        # return total
 
-            for j in range(i, len(arr)):
-                current_sum += arr[j]
-
-                length = j - i + 1
-
-                if length % 2 == 1:
-                    total += current_sum
-
-        return total
+        res = 0
+        n = len(arr)
+        for idx,num in enumerate(arr):
+            res += ((idx+1)*(n-idx)+1)//2*num
+        return res
