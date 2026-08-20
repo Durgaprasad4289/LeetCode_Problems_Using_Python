7e21f2853ef0fd0ade3e0d97c5c1471ac853dc62
Durga Prasad Kota <durgaprasad04289@gmail.com>
2026-08-20 21:50:53 +0530

    1652. Defuse the Bomb - Version-1 O(n)

--- HashMap + PrefixSum/Defuse the Bomb.py
+++ HashMap + PrefixSum/Defuse the Bomb.py
@@ -1,3 +1,4 @@
+# ---------- VERSION 1 O(n x |k|) ---------
 class Solution:
     def decrypt(self, code: List[int], k: int) -> List[int]:
         if k == 0:
@@ -18,4 +19,30 @@ class Solution:
                     idx = (i-j) % n
                     t += code[idx]
                 res.append(t)
+        return res
+
+# ---------- VERSION-2  O(n) ---------
+
+class Solution:
+    def decrypt(self, code: List[int], k: int) -> List[int]:
+
+        n = len(code)
+        if k == 0:
+            return [0]*n
+
+        res = [0]*n
+        
+        if k>0:
+            window_sum = sum(code[j%n] for j in range(1,k+1))
+            for i in range(n):
+                res[i] = window_sum
+                window_sum -= code[(i+1)%n]
+                window_sum += code[(i+k+1)%n]
+        else :
+            k = -k
+            window_sum = sum(code[-j%n] for j in range(1,k+1))
+            for i in range(n):
+                res[i] = window_sum
+                window_sum -= code[(i-k)%n]
+                window_sum += code[i%n]
         return res
